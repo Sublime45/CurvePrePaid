cab21d521de25876348b32526d93fb3dde76b529
Dimitrios Elissaios <dimitrios.elissaios@gmail.com>
2018-03-23 12:53:40 -0700

    Created an abstract class from which different data access object classes can be derived

---
+++ src/Main/DataAccess/BaseDAO.java
@@ -0,0 +1,29 @@
+package Main.DataAccess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public abstract class BaseDAO<T> {
+	protected final String tableName;
+	protected Connection connection;
+	
+	protected BaseDAO(Connection connection, String tableName) {
+		this.tableName = tableName;
+		this.connection = connection;
+	}
+	
+	protected ResultSet executeQuery(String query) throws SQLException {
+		PreparedStatement stmnt = this.connection.prepareStatement(query);
+		ResultSet rs = stmnt.executeQuery();
+		return rs;
+	}
+	
+    public abstract List<T> get() throws SQLException;
+    public abstract T getById(int id) throws SQLException;
+    public abstract void insert(T row) throws SQLException; 
+    public abstract void update(T row) throws SQLException; 
+
+}
