e56a509ca00fa354828cd2ab4bde704fa56880f7
Georgios Elissaios <geo@Georgioss-MacBook-Pro.local>
2018-03-23 12:50:53 -0700

    Added the User model that represents the users table

---
+++ src/Main/Models/User.java
@@ -0,0 +1,22 @@
+package Main.Models;
+
+public class User {
+	private int id;
+	private float balance;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public float getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+}
